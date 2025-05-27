7a61e5690be8dab948fbe40cf4b4426ac81c88c6
Alexander Adam <alexadam340@yahoo.gr>
2025-05-27 23:59:14 +0300

    Complete experiments and divide data into 4 partitions (#2)
    Co-authored-by: alexandrosadam <alexadam340@yahoo.com>

--- src/main.py
+++ src/main.py
@@ -7,25 +7,26 @@ from group_runner import GroupRunner
 
 
 def main():
-    if len(sys.argv) != 2:  # Ensure no extra args are passed
-        print("Error: Exactly one argument required: 'naive' or 'group'.")
-        sys.exit(1)
-
     # In case a user execute main file with --help flag
-    parser = argparse.ArgumentParser(description="Run Spark job with 'naive' or 'group' argument")
-    parser.add_argument("mode", nargs=1, help="Execution mode: naive or group of Map Reduce implementation")
+    parser = argparse.ArgumentParser(
+        description="Run Spark job with 'naive' or 'group' argument and 'small', 'medium' or 'large' for dataset")
+    parser.add_argument("mode", help="Execution mode: naive or group of Map Reduce implementation")
+    parser.add_argument("dataset_size", help="Dataset size: small, medium, or large")
 
     args = parser.parse_args()
     start_time = time.time()
 
-    if args.mode[0] == "naive":
-        NaiveRunner.execute_naive_all_pairs_matching()
+    valid_dataset_sizes = {"small", "medium", "large"}
+
+    if args.mode == "naive" and args.dataset_size in valid_dataset_sizes:
+        NaiveRunner.execute_naive_all_pairs_matching(args.dataset_size)
         print(f"Naive approach completed in {time.time() - start_time:.2f} seconds", flush=True)
-    elif args.mode[0] == "group":
-        GroupRunner().execute_group_all_pairs_matching()
+    elif args.mode == "group" and args.dataset_size in valid_dataset_sizes:
+        GroupRunner().execute_group_all_pairs_matching(args.dataset_size)
         print(f"Group approach completed in {time.time() - start_time:.2f} seconds", flush=True)
     else:
-        print(f"Error: Invalid argument '{args.mode[0]}'. Use 'naive' or 'group'.", flush=True)
+        print(f"Error: Invalid mode argument or dataset argument. Run with --help flag to get help information!",
+              flush=True)
         sys.exit(1)
 
 
